ea8650194bb72aafbcba6b0eda0565d3b83569e4
Carl Osterwisch <costerwi@gmail.com>
2024-10-12 14:07:08 -0400

    Refactor to work with edgeArray of tangent edges instead of edge

--- bolting.py
+++ bolting.py
@@ -16,11 +16,12 @@ import numpy as np
 from scipy.spatial import KDTree
 
 
-def uniqueKey(base, repository):
+def uniqueKey(repository, baseName='Item'):
+    "Return a new unique key within the repository"
     n = 0
     while 0 == n or name in repository:
         n -= 1
-        name = base + str(n)
+        name = baseName + str(n)
     return name
 
 
@@ -29,44 +30,53 @@ def referencePair(rp1, rp2):
     return tuple(sorted([rp1, rp2]))
 
 
+def tangentEdges(edge):
+    "Return edgeArray of tangent edges with the same radius as edge"
+    radius = edge.getRadius()
+    edgeArray = edge.getEdgesByEdgeAngle(1.0) # tangent edges within 1 degree
+    for edge in edgeArray:
+        r = edge.getRadius()
+        if abs(r - radius)/radius > 0.01:
+            raise ValueError('Tangent edges have inconsistent radii')
+    return edgeArray
+
+
 def getSimlarEdges(rootAssembly, edge0):
-    "Find similar size circular edges in all instances of this Part"
-    #TODO return list of edgeArray masks
+    "Return list of edge arrays with same radius as edge0 in all instances of this Part"
     radius = edge0.getRadius()
-    similarEdges = [edge0] # Similar size edges within the original instance
-    instance = rootAssembly.instances[edge0.instanceName]
-    for edge in instance.edges:
+    instance0 = rootAssembly.instances[edge0.instanceName]
+    instance0Edges = [tangentEdges(edge0)] # edge0 forms the first item in the list
+    for edge in instance0.edges:  # search edges within original instance
         if edge.isReferenceRep:
             continue # reference geom
-        if edge.index == edge0.index:
-            continue # original edge
-        if len(edge.getVertices()) > 1:
-            continue # incomplete circle TODO consider circles made up of multiple edges
-        #f = edge.getFaces()
-        #if len(f) > 1 or f[0] != faces[0]:
-        #    continue # in case of sketch editing
+        if any(edge in edgeArray for edgeArray in instance0Edges):
+            continue # already in the list
         try:
             r2 = edge.getRadius()
-        except Exception as E:
+        except:
             continue # not recognized as an arc
         if abs((radius - r2)/radius) > 0.01:
             continue # wrong radius
-        similarEdges.append(edge)
+        try:
+            instance0Edges.append(tangentEdges(edge))
+        except:
+            continue # failed to construct tangent edges
 
-    # Add the same edges from all instance of this part
-    copiedEdges = similarEdges.copy()
+    # Add the same edges arrays from all instance of this part
+    allSimilarEdges = instance0Edges.copy()
     for otherInstance in rootAssembly.instances.values():
         if not hasattr(otherInstance, 'partName'):
             continue # assembly instance
+        if otherInstance.partName != instance0.partName:
+            continue # different part
+        if otherInstance.name == instance0.name:
+            continue # same instance
         if rootAssembly.features[otherInstance.name].isSuppressed():
             continue # suppressed instance
-        if otherInstance.name == instance.name:
-            continue # same instance
-        if otherInstance.partName != instance.partName:
-            continue # different part
-        similarEdges.extend(otherInstance.edges[e.index] for e in edges)
+        allSimilarEdges.extend(otherInstance.edges.getSequenceFromMask(
+                edgeArray.getMask()) for edgeArray in instance0Edges)
 
-    return copiedEdges
+    return allSimilarEdges
 
 
 def hashEdge(edge):
@@ -74,10 +84,11 @@ def hashEdge(edge):
     return edge.instanceName, edge.index
 
 
-def centerPoint(model, edge):
+def centerPoint(model, edgeArray):
     "Create reference point at center of circle and connect with coupling"
     rootAssembly = model.rootAssembly
-    # Search all existing couplings for this edge, return its referencePoint
+    # Search all existing couplings for this edge, return its referencePoint if found
+    edge0 = min(edgeArray)
     for constraint in model.constraints.values():
         if not hasattr(constraint, 'couplingType'):
             continue # not a coupling
@@ -88,9 +99,7 @@ def centerPoint(model, edge):
             surface = rootAssembly.allInternalSurfaces[surfaceName]
         else:
             surface = rootAssembly.surfaces[surfaceName]
-        if len(surface.edges) != 1:
-            continue
-        if hashEdge(edge) != hashEdge(surface.edges[0]):
+        if not edge0 in surface.edges:
             continue # not the same edge
         controlSetName = constraint.controlPoint[0]
         if controlSetName in rootAssembly.allInternalSets:
@@ -103,16 +112,14 @@ def centerPoint(model, edge):
         return controlSet.referencePoints[0]
 
     # Create a new reference point and coupling constraint
-    instance = rootAssembly.instances[edge.instanceName]
-    rpFeature = rootAssembly.ReferencePoint(point=instance.InterestingPoint(edge, CENTER))
+    instance = rootAssembly.instances[edge0.instanceName]
+    rpFeature = rootAssembly.ReferencePoint(point=instance.InterestingPoint(edge0, CENTER))
     rp = rootAssembly.referencePoints[rpFeature.id]
     rootAssembly.features.changeKey(fromName=rpFeature.name,
-                                    toName=uniqueKey('BoltRP', rootAssembly.features))
+                                    toName=uniqueKey(rootAssembly.features, 'BoltRP'))
     controlRegion = regionToolset.Region( referencePoints=[rp] )
-    surfaceRegion = regionToolset.Region(side1Edges=instance.edges[edge.index:edge.index+1])
-    #surfaceRegion = regionToolset.Region(side1Edges=([edge],) )
-    #surfaceRegion = regionToolset.Region(side1Edges=[instance.edges[edge.index]] )
-    model.Coupling(name=uniqueKey('BoltCoupling', model.constraints),
+    surfaceRegion = regionToolset.Region(side1Edges=edgeArray)
+    model.Coupling(name=uniqueKey(model.constraints, 'BoltCoupling'),
         controlPoint=controlRegion,
         surface=surfaceRegion,
         influenceRadius=WHOLE_SURFACE,
@@ -122,19 +129,20 @@ def centerPoint(model, edge):
 
 
 connectedPoints = []  # make sure an edge is only added once
-def wireBetweenEdgeCenters(model, edgeA, edgeB):
+def wireBetweenEdgeCenters(model, edgeArrayA, edgeArrayB):
     "Create a wire feature between center of edgeA and edgeB"
 
     rootAssembly = model.rootAssembly
-    rpA = centerPoint(model, edgeA)
-    rpB = centerPoint(model, edgeB)
+    rpA = centerPoint(model, edgeArrayA)
+    rpB = centerPoint(model, edgeArrayB)
     pairId = referencePair(rpA, rpB)
     if pairId in connectedPoints:
         return None # wire already exists
 
     wire = rootAssembly.WirePolyLine(points=((rpA, rpB), ), meshable=False)
     connectedPoints.append(pairId)
-    newName = uniqueKey('Bolt-{}-{}'.format(edgeA.instanceName, edgeB.instanceName), rootAssembly.features)
+    newName = uniqueKey(rootAssembly.features,
+            'Bolt-{}-{}'.format(min(edgeArrayA).instanceName, min(edgeArrayB).instanceName))
     rootAssembly.features.changeKey(fromName=wire.name, toName=newName)
     return rootAssembly.features[newName]
 
@@ -142,24 +150,20 @@ def wireBetweenEdgeCenters(model, edgeA, edgeB):
 def addConnectors(edge1, edge2):
     "Main method called by CAE"
 
-    # Check for bad input from user
     viewport = session.viewports[session.currentViewportName]
     rootAssembly = viewport.displayedObject
     model = mdb.models[rootAssembly.modelName]
 
+    # Check for bad input from user
     if edge1.instanceName == edge2.instanceName:
         raise ValueError('Edges are from the same instance')
     edges = edge1, edge2
     radii = [edge.getRadius() for edge in edges] # will raise exception if not a radius
-    if any(r < 1e-3 for r in radii):
-        raise ValueError('Radius is very small')
     for edge in edges:
-        if len(edge.getVertices()) > 1:
-            raise ValueError('Edge {} forms incomplete circle'.format(edge.instanceName))
         instance = rootAssembly.instances[edge.instanceName]
         assert hasattr(instance, 'partName')
 
-    # Collect uniqueId of all existing wires
+    # Collect connectedPoints of existing wires to avoid duplication
     connectedPoints.clear()
     for edge in rootAssembly.edges:
         vertexList = edge.getVertices()
@@ -169,21 +173,27 @@ def addConnectors(edge1, edge2):
         rp = (rootAssembly.referencePoints.findAt(*v.pointOn) for v in vertices) # TODO is findAt most efficient?
         connectedPoints.append(referencePair(*rp))
 
-    wires = [wireBetweenEdgeCenters(model, edge1, edge2)]
-    distance0 = np.linalg.norm(np.asarray(edge1.pointOn[0]) - edge2.pointOn[0])
-
     similarEdges1 = getSimlarEdges(rootAssembly, edge1)
     similarEdges2 = getSimlarEdges(rootAssembly, edge2)
 
-    # Find and connect similar sized circular edges on all instances of the two parts
-    pointTree = KDTree([e.pointOn[0] for e in similarEdges2])
-    distances, index2 = pointTree.query([e.pointOn[0] for e in similarEdges1])
+    distance0 = np.linalg.norm(np.asarray(edge1.pointOn[0]) - edge2.pointOn[0])
     maxDistance = 1.5*(distance0 + sum(radii))
-    sortedRows = np.argsort(distance)
-    for row in sortedRows:
-        if distance[row] > maxDistance:
-            break # this row and remaining rows are too far away
-        wires.append(wireBetweenEdgeCenters(model, similarEdges1[row], similarEdges2[index2[row]]))
+
+    # Find edges in similarEdges2 closest to simpiarEdges1
+    pointTree = KDTree([min(edgeArray).pointOn[0] for edgeArray in similarEdges2])
+    distances, index2 = pointTree.query([min(edgeArray).pointOn[0] for edgeArray in similarEdges1],
+                                        distance_upper_bound=maxDistance)
+
+    row2Points = set() # keep track to prevent multiple wires connecting to the same edge2 similar edge
+    wires = []
+    for row in np.argsort(distances):
+        if distances[row] > maxDistance:
+            break # edge arrays in this row and remaining rows are too far away from each other
+        row2 = index2[row]
+        if row2 in row2Points:
+            continue # already connected to this edgeArray; shortest distance wins
+        row2Points.add(row2)
+        wires.append(wireBetweenEdgeCenters(model, similarEdges1[row], similarEdges2[row2]))
 
     wireNames = set(w.name for w in wires if w is not None)
     geomsequence = None
@@ -199,7 +209,7 @@ def addConnectors(edge1, edge2):
         print('No new wires added from hole diameter {:.3g} to hole diameter {:.3g}'.format(
             2*radii[0], 2*radii[1]))
     else:
-        name = uniqueKey('BoltWires', rootAssembly.sets)
+        name = uniqueKey(rootAssembly.sets, 'BoltWires')
         rootAssembly.Set(name=name, edges=geomsequence)
 
         print(len(geomsequence), name,
